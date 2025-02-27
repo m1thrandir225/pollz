887d182936bff3bc4e5bb77711698dac23bced0d
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2025-02-27 13:19:52 +0100

    feat: main file gin server setup

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"m1thrandir225/cicd2025/api"
+)
 
 func main() {
 	fmt.Println("Hello World")
+}
+
+func runGinServer() {
+	server, err := api.NewServer()
+	if err != nil {
+		log.Fatal("Cannot create server")
+	}
+	err = server.Start("http://localhost:8080")
+	if err != nil {
+		log.Fatal("Cannot start server")
+	}
 }
