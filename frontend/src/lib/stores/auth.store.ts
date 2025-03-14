f0544112a83c3d2ccc5fac88f6f3e9515881c020
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2025-03-14 16:36:35 +0100

    feat: frotnend use vue instead of svelte

--- frontend/src/lib/stores/auth.store.ts
+++
@@ -1,9 +0,0 @@
-import { writable} from "svelte/store";
-import { browser } from "$app/environment";
-
-export const isAuthenticated = writable(false);
-export const user = writable(null);
-
-export async function checkAuth() {
-    
-}
