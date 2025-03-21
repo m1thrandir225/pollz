bc386d77f0a4707b2b727f7aecce889032e4b3f0
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2025-03-21 21:07:47 +0100

    test: db query tests for votes

--- backend/db/sqlc/votes_test.go
+++ backend/db/sqlc/votes_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"m1thrandir225/cicd2025/util"
 	"testing"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
@@ -37,8 +38,62 @@ func TestCreateVote(t *testing.T) {
 	createRandomVote(t, option.ID)
 }
 
-func TestGetVote(t *testing.T) {}
+func TestGetVote(t *testing.T) {
+	user := createRandomUser(t)
+	poll := createRandomPoll(t, user.ID)
+	option := createRandomPollOption(t, poll.ID)
+	vote := createRandomVote(t, option.ID)
 
-func TestUpdateVote(t *testing.T) {}
+	result, err := testStore.GetVote(context.Background(), vote.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, result)
 
-func TestDeleteVote(t *testing.T) {}
+	require.Equal(t, vote.ID, result.ID)
+	require.Equal(t, vote.IpAddress, result.IpAddress)
+	require.Equal(t, vote.OptionID, result.OptionID)
+	require.WithinDuration(t, vote.VotedAt, result.VotedAt, time.Millisecond)
+}
+
+func TestUpdateVote(t *testing.T) {
+	user := createRandomUser(t)
+	poll := createRandomPoll(t, user.ID)
+	option := createRandomPollOption(t, poll.ID)
+	vote := createRandomVote(t, option.ID)
+
+	newOption := createRandomPollOption(t, poll.ID)
+	arg := UpdateVoteOptionParams{
+		ID:       vote.ID,
+		OptionID: newOption.ID,
+	}
+
+	updated, err := testStore.UpdateVoteOption(t.Context(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, updated)
+
+	require.Equal(t, vote.ID, updated.ID)
+	require.Equal(t, vote.IpAddress, updated.IpAddress)
+	require.NotEqual(t, vote.OptionID, updated.OptionID)
+	require.WithinDuration(t, vote.VotedAt, updated.VotedAt, time.Millisecond)
+
+	require.Equal(t, updated.OptionID, arg.OptionID)
+}
+
+func TestDeleteVote(t *testing.T) {
+	user := createRandomUser(t)
+	poll := createRandomPoll(t, user.ID)
+	option := createRandomPollOption(t, poll.ID)
+	vote := createRandomVote(t, option.ID)
+
+	deleted, err := testStore.DeleteVote(context.Background(), vote.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, deleted)
+
+	require.Equal(t, vote.ID, deleted.ID)
+	require.Equal(t, vote.IpAddress, deleted.IpAddress)
+	require.Equal(t, vote.OptionID, deleted.OptionID)
+	require.WithinDuration(t, vote.VotedAt, deleted.VotedAt, time.Millisecond)
+
+	result, err := testStore.GetVote(context.Background(), deleted.ID)
+	require.Error(t, err)
+	require.Empty(t, result)
+}
