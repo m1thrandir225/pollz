9b4188381b780586f49168bcaab8bd49236cb532
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2025-06-30 23:38:55 +0200

    feat: added ingress for kubernetes

--- scripts/start-kubernetes.sh
+++ scripts/start-kubernetes.sh
@@ -1,8 +1,4 @@
-k3d cluster create pollz-local \
-  --port "3000:30000@loadbalancer" \
-  --port "8081:30001@loadbalancer" \
-  --port "5432:30002@loadbalancer" \
-  --agents 2
+k3d cluster create pollz-local --agents 2 -p "80:80@loadbalancer" -p "443:443@loadbalancer"
 
 kubectl config current-context
 
@@ -10,7 +6,7 @@ kubectl apply -f namespace.yaml
 kubectl apply -f configmap.yaml
 kubectl apply -f secret.yaml
 kubectl apply -f pvc.yaml
-kubectl apply -f db-deployment.yaml
+kubectl apply -f db-statefulset.yaml
 kubectl apply -f db-service.yaml
 
 kubectl apply -f backend-deployment.yaml
@@ -18,4 +14,4 @@ kubectl apply -f backend-service.yaml
 kubectl apply -f frontend-deployment.yaml
 kubectl apply -f frontend-service.yaml
 
-kubectl get pods -n pollz -w
+kubectl apply -f ingress.yaml
